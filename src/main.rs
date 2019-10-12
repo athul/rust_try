b5e2056ef5e2643f9c5b07783e93cfc7d6e8e926
athul <athul8720@gmail.com>
2019-10-12 22:21:49 +0530

    Loop and Breaking

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::cmp::Ordering;
 use rand::Rng;
 
 fn main(){
+    loop{
 println!("Guess the Number");
 let secret_number = rand::thread_rng().gen_range(0,101);
 
@@ -14,10 +15,13 @@ io::stdin().read_line(&mut guess)
 let guess: u32 = guess.trim().parse()
         .expect("Please type a number!");
 match guess.cmp(&secret_number){
-    Ordering::Less=>println!("Less"),
-    Ordering::Equal=>println!("Yay You're good"),
-    Ordering::Greater=>println!("You're a rocket boi!!!"),
+    Ordering::Less=>println!("--------Less"),
+    Ordering::Greater=>println!("-------You're a rocket boi!!!"),
+    Ordering::Equal=>{
+        println!("--------Yay You're good");
+        break;}
 }
 println!("The secret number is {}",secret_number);
 println!("You guessed:{}",guess);
+    }
 }

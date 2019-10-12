c4032c460fdd990b519b5a01f10dcd510a1b55f7
athul <athul8720@gmail.com>
2019-10-12 20:54:10 +0530

    Added Checking for Secret Nos

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main(){
@@ -10,6 +11,13 @@ println!("What's your Guess?");
 io::stdin().read_line(&mut guess)
     .expect("Failed to Read Line");
 
+let guess: u32 = guess.trim().parse()
+        .expect("Please type a number!");
+match guess.cmp(&secret_number){
+    Ordering::Less=>println!("Less"),
+    Ordering::Equal=>println!("Yay You're good"),
+    Ordering::Greater=>println!("You're a rocket boi!!!"),
+}
 println!("The secret number is {}",secret_number);
 println!("You guessed:{}",guess);
 }

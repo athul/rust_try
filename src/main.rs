0ecada2b667801a76a4dd0dfe1a10826173b4c76
athul <athul8720@gmail.com>
2019-10-12 20:27:50 +0530

    New Secret Number Generator

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use std::io;
+use rand::Rng;
 
 fn main(){
 println!("Guess the Number");
-println!("Input your guess");
+let secret_number = rand::thread_rng().gen_range(0,101);
 
 let mut guess=String::new();
-
+println!("What's your Guess?");
 io::stdin().read_line(&mut guess)
     .expect("Failed to Read Line");
 
+println!("The secret number is {}",secret_number);
 println!("You guessed:{}",guess);
 }

482ad3c4256747930377c71cc2f5764ce46c9ee3
athul <athul8720@gmail.com>
2019-10-13 14:56:52 +0530

    Completed the Stuff

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,31 @@
-use std::io;
-use std::cmp::Ordering;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
-fn main(){
-    loop{
-println!("Guess the Number");
-let secret_number = rand::thread_rng().gen_range(0,101);
+fn main() {
+    loop {
+        println!("Guess the Number");
+        let secret_number = rand::thread_rng().gen_range(0, 101);
 
-let mut guess=String::new();
-println!("What's your Guess?");
-io::stdin().read_line(&mut guess)
-    .expect("Failed to Read Line");
+        let mut guess = String::new();
+        println!("What's your Guess?");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to Read Line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-let guess: u32 = guess.trim().parse()
-        .expect("Please type a number!");
-match guess.cmp(&secret_number){
-    Ordering::Less=>println!("--------Less"),
-    Ordering::Greater=>println!("-------You're a rocket boi!!!"),
-    Ordering::Equal=>{
-        println!("--------Yay You're good");
-        break;}
-}
-println!("The secret number is {}",secret_number);
-println!("You guessed:{}",guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("--------Less"),
+            Ordering::Greater => println!("-------You're a rocket boi!!!"),
+            Ordering::Equal => {
+                println!("--------Yay You're good");
+                break;
+            }
+        }
+        println!("The secret number is {}", secret_number);
+        println!("You guessed:{}", guess);
     }
 }
